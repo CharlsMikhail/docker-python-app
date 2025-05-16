b25b551b8a01d0be9263ad34a9e16b97f17bcb96
CharlsMikhail <charlsmikhail@serveraco.softcram>
2025-05-16 12:08:11 -0500

    Primer commit con workflow de Docker

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hola Mundo\n")
+        self.wfile.write(b"Hola Mundo cambiando\n")
 
 if __name__ == "__main__":
     server_address = ("0.0.0.0", 8097)
